16a09b29aaed5023a693e260fcb3a9d79a5a3ec6
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-10-19 17:25:31 +0100

    added file

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-$(document).ready(function(){
-    $("#btnlink, #menu-btn").click(function(){
-      $("#menu").addClass("enabled");
-    
-    });
-
-    $("#about-btn").click(function(){
-      $("#about").addClass("enabled");
-    });
-
-    $("#interior-btn").click(function(){
-      $("#interior").addClass("enabled");
-    });
-
-    $("#contact-btn").click(function(){
-      $("#contact").addClass("enabled");
-    });
-
-  });
+document.addEventListener('DOMContentLoaded',function(){
+        // GET Request.
+fetch('https://juniors-restaurant.glitch.me/api/order')
+    // Handle success
+    .then(response => response.json())  // convert to json
+    .then(data => java(data))    //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors
+   
+ 
+       });
+         function java(data){
+           var html="";var tab;
+         data.map(d=>{tab =`<div class="grid-item"><img class="images" src=${d.image} alt=${d.name}/><div class="btn-container">${d.name}<button class="btn2">Order Now</button></div></div>`;html +=tab})
+       document.querySelector(".grid-container").innerHTML = html;  
+        }
